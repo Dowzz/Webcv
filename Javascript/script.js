e5cdc31ebcd7b1ae5494152a2ca241c324eaba58
Dowzz <michel.barny@outlook.fr>
2021-07-28 19:58:29 +0200

    correction
    Signed-off-by: Dowzz <michel.barny@outlook.fr>

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,6 +5,7 @@ $(() =>{
 
     window.addEventListener('load', function(){
         boiteVideo.style.display="none";
+        $('.content').load("Pages/accueil.html")
     })
 $('#showVid').click(function(){
     $('#boiteVideo').fadeIn(400);
@@ -15,13 +16,26 @@ $('#hideVid').click(function(){
 $(function(){
     $(document.getElementsByClassName("titre_Partie")).click(function(e){
         e.preventDefault();
-        url=("Pages/"+this.id+".html");
+        url=(this.href);
         $.get(url, function(data) {
             $('.content').html(data);
         })
     })
 })
-
+function showResponsiveMenu() {
+    var menu = document.getElementById("navmenu_reponsive");
+    var icon = document.getElementById("nav_hamburger_icon");
+    var root = document.getElementById("root");
+    if (menu.className === "") {
+        menu.className = "open";
+        icon.className = "open";
+        root.style.overflowY = "hidden";
+    } else {
+        menu.className = "";                    
+        icon.className = "";
+        root.style.overflowY = "";
+    }
+}
 });
 
 
