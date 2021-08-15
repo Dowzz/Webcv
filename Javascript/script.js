6054b27e149d093028bf8427c5cc5823aa7f9a57
Dowzz <michel.barny@outlook.fr>
2021-08-16 00:37:02 +0200

    grosse mise a jour
    Signed-off-by: Dowzz <michel.barny@outlook.fr>

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,17 +5,12 @@ $(() =>{
     window.addEventListener('load', function(){
         $('.content').load("Pages/accueil.html")
     })
-$('#showVid').click(function(){
-    $('#boiteVideo').fadeIn(400);
-})
-$('#hideVid').click(function(){
-    $('#boiteVideo').fadeOut(400);
-})
 $(function(){
     $(document.getElementsByClassName("titre_Partie")).click(function(e){
         e.preventDefault();
         url=(this.href);
         $.get(url, function(data) {
+            $('.content').addClass('detract')
             $('.content').html(data);
             if (window.matchMedia("(max-width: 1250px)").matches) {
                 document.getElementById('check')
@@ -25,8 +20,17 @@ $(function(){
     })
 })
 });
+function remove(id){
+    if(id === (document.getElementById(id))){
+        return
+    }else{
+        $('.content').removeClass("detract");
+    }
+
+}
 function toggleMenu() {
     var x = document.getElementById("myLinks");
     $(x).fadeToggle();
-  }
+}
+
 
